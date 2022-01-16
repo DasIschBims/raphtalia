b532ebf1405d340c26cbb8247b1caa764d918e55
DasIschBims <you@example.com>
2022-01-16 22:02:28 +0100

    Edited interaction error msg

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ client.on('interactionCreate', async interaction => {
         await command.execute(interaction)
     } catch (error) {
         console.error(error)
-        await interaction.reply({ content: 'An error occured while attemting to run this command.', ephemeral: true})
+        await interaction.reply({ content: `An error occured while attemting to run /${interaction.commandName}.\nPlease try again, if this doesn't work try contacting DasIschBims#1248 via the [Support Server](https://discord.gg/ZURcscg)`, ephemeral: true})
     }
 });
 
