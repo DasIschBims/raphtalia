eaf8b93fc3d5d020c87e827ad14f27abe16a7137
DasIschBims <gilmann.florian@gmail.com>
2022-01-23 12:22:47 +0100

    Fixed buttons being clickable with opacity: 0

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ function navButton (btn) {
     btn.classList.toggle("navMenuActive")
     document.getElementById("navMenuOverlay").classList.toggle("activeOverlay")
     document.body.classList.toggle("hideOverflow")
+    document.getElementById("overlayContent").classList.toggle("displayNone")
 }
