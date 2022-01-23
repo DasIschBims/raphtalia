20b76e7e7aa3dded31ac65ba8de538065c6ce212
DasIschBims <gilmann.florian@gmail.com>
2022-01-23 01:34:02 +0100

    Finished responsive navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 function navButton (btn) {
     btn.classList.toggle("navMenuActive")
+    document.getElementById("navMenuOverlay").classList.toggle("activeOverlay")
+    document.body.classList.toggle("hideOverflow")
 }
