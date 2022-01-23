35c71b8bc8c73c6241b1e9baa92bc63545957b08
DasIschBims <gilmann.florian@gmail.com>
2022-01-23 13:09:24 +0100

    Fixed navoverlay staying on top

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@ function navButton (btn) {
     btn.classList.toggle("navMenuActive")
     document.getElementById("navMenuOverlay").classList.toggle("activeOverlay")
     document.body.classList.toggle("hideOverflow")
-    document.getElementById("overlayContent").classList.toggle("displayNone")
+}
+
+function overlayMenu () {
+    document.getElementById("navMenuOverlay").classList.remove("displayNone")
 }
